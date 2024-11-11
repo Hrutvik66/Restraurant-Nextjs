97f7966a1e00be1952c760c54a7253b2474a856e
Hrutvik66 <hrutvikmalshikare66@gmail.com>
2024-11-11 23:48:15 +0530

    Redis is removed temporarily

--- restraurant-backend/src/utils/redisClient.ts
+++
@@ -1,9 +0,0 @@
-import Redis from "ioredis";
-
-const redisClient = new Redis({
-  host: "localhost", // Replace with your Redis host
-  port: 6379, // Replace with your Redis port
-  db: 0, // Replace with your Redis database index
-});
-
-export default redisClient;
